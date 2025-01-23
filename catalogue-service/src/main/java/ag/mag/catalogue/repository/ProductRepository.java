0a29eb15a45be6fe30969590b6b16481cb9e341e
Dyglaz <kallkae@mail.ru>
2025-01-23 23:57:01 +0300

    chore(catalogue-service): Remove InMemoryProductRepository and update ProductRepository interface

--- catalogue-service/src/main/java/ag/mag/catalogue/repository/ProductRepository.java
+++ catalogue-service/src/main/java/ag/mag/catalogue/repository/ProductRepository.java
@@ -1,17 +1,8 @@
 package ag.mag.catalogue.repository;
 
 import ag.mag.catalogue.entity.Product;
+import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-import java.util.Optional;
+public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-public interface ProductRepository {
-
-    List<Product> findAll();
-
-    Product save(Product product);
-
-    Optional<Product> findById(Integer productId);
-
-    void deleteById(Integer id);
 }
